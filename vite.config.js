9a88ee33ff5764031a3a93449ab57712241b367e
aksri648 <akshat.23b0131290@abes.ac.in>
2026-03-31 20:11:15 +0530

    rollback

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  // Relative assets make the same build work on both custom domain and project pages URL.
-  base: './',
+  base: '/vistaran_exports2/',
   plugins: [react()],
 })
